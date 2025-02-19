1a03df270fa21b8cf72af2366079d40c1ad93194
jacoubakiz <J.akizi@icsl.me>
2025-02-19 16:11:31 +0300

    updagte

--- api/pagination.py
+++ api/pagination.py
@@ -1,6 +1,14 @@
 from rest_framework.pagination import PageNumberPagination
-
+from rest_framework.response import Response
 class CustomPaginatins(PageNumberPagination):
     page_size = 50
     page_size_query_param = 'size_page'
     max_page_size = 600
+
+    def get_paginated_response(self, data):
+        return Response({
+            "count": self.page.paginator.count,
+            "next": self.get_next_link()[28:],
+            "previous": self.get_previous_link()[28:],
+            "results": data
+        })

259e7ade93fc37ad45b41ae7ea7ebf3354220f1d
jacoubekiz <J.akizi@icsl.me>
2025-07-30 16:03:35 +0300

    update

--- api/consumers.py
+++ api/consumers.py
@@ -528,8 +528,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             content_type = content_type,
             content = content,
             wamid = wamid,
-            media_url = media_url,
-            status_message = status,
+            media_url = media_url
         )
         return chat_message.message_id
     

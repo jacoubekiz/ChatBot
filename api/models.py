34c8cf887a6dc037b70197669ebef457d23f6d07
jacoubekiz <J.akizi@icsl.me>
2026-01-28 00:05:11 +0300

    update

--- api/models.py
+++ api/models.py
@@ -424,7 +424,7 @@ class Conversation(models.Model):
 class QuickReply(models.Model):
     quickreply_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    payload = models.CharField(max_length=255)
+    payload = models.CharField(max_length=255, null=True, blank=True)
     image = models.ImageField(upload_to='quick_replies/', null=True, blank=True)
     account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)

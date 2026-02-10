57b6ad193cc39bad3e55670e019a8ed609696a41
jacoubekiz <J.akizi@icsl.me>
2026-02-10 19:49:27 +0300

    update

--- api/utils.py
+++ api/utils.py
@@ -613,13 +613,12 @@ def handel_request_redis(data, account_id):
                                 sent_message_text(conversation.conversation_id, content, content_type, wamid, chat_message.message_id, chat_message.created_at, contact.phone_number, channel.channle_id)
                             case "text":
                                 content = value.get('messages', '')[0].get('text', '').get('body','')
-                                print(conversation.contact_id.name)
                                 chat_message = ChatMessage.objects.create(
                                     conversation_id = conversation,
                                     # user_id = CustomUser1.objects.filter(id=15).first(),
                                     content_type = content_type,
                                     content = content,
-                                    from_message = conversation.contact_id.name,
+                                    from_message = conversation.contact_id.name or contact.phone_number,
                                     wamid = wamid
                                 )
                                 sent_message_text(conversation.conversation_id, content, content_type, wamid, chat_message.message_id, chat_message.created_at, contact.phone_number, channel.channle_id)

c5455660183878d8efa4251a4a697f3bebcd3f7a
jacoubekiz <J.akizi@icsl.me>
2026-08-05 15:15:19 +0300

    update

--- api/Utility/UtilsWebhook/webhook_handlers.py
+++ api/Utility/UtilsWebhook/webhook_handlers.py
@@ -182,6 +182,8 @@ def handle_incoming_message(value: dict) -> dict:
     # Check for restart keyword
     restart_keywords = get_restart_keywords(channel.channle_id)
     content = message_data.get('text', message_data.get('button', message_data.get('interactive', '')))
+    with open('content_user_reply.txt', 'a') as ff:
+        ff.write(f'start bot with: {content}\n')
     
     if content in restart_keywords:
         conversation.state = 'start_bot'
